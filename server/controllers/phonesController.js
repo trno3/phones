dc157f30069a37925f06cf4215ebaf092f63b69c
toni <toni_hp@hotmail.com>
2021-11-19 23:09:09 +0100

    Added two more fake phones to test

--- server/controllers/phonesController.js
+++ server/controllers/phonesController.js
@@ -24,6 +24,30 @@ const phonesList = (_req, res) => {
             processor: 'A11',
             ram: 2,
         },
+        {
+            id: 0,
+            name: 'iPhone 7',
+            manufacturer: 'Apple',
+            description: 'lorem ipsum dolor sit amet consectetur.',
+            color: 'black',
+            price: 769,
+            imageFileName: 'IPhone_7.png',
+            screen: '4,7inch IPS',
+            processor: 'A10Fusion',
+            ram: 2,
+        },
+        {
+            id: 1,
+            name: 'iPhone 8',
+            manufacturer: 'Apple',
+            description: 'lorem ipsum dolor sit amet consectetur.',
+            color: 'black',
+            price: 969,
+            imageFileName: 'IPhone_8.png',
+            screen: '5inch IPS',
+            processor: 'A11',
+            ram: 2,
+        },
     ];
 
     res.send(JSON.stringify(data));
