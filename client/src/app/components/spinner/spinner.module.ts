f573380cef735fe7a16c6045a7f479ffa497ab2a
toni <toni_hp@hotmail.com>
2021-11-20 11:13:43 +0100

    added a spinner component

---
+++ client/src/app/components/spinner/spinner.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { SpinnerComponent } from './spinner.component';
+
+@NgModule({
+  exports: [SpinnerComponent],
+  declarations: [SpinnerComponent],
+})
+export class SpinnerModule {}
