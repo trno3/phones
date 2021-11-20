a83a580ef593fa02d362d65740b2e825e39bc9cf
toni <toni_hp@hotmail.com>
2021-11-20 15:46:42 +0100

    Set angular build into the dist folder into server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('diskdb');
 const phonesRoutes = require('./routes/phonesRouter');
+const path = require('path');
 
 db.connect('./server/data', ['phones.json']);
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use('/phones', phonesRoutes);
+
+app.use(express.static(path.join(__dirname, 'dist/client')));
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
