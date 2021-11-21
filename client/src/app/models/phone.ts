5c8df3e072fd4de4c51cdcf49dca0baf9a7d713f
toni <toni_hp@hotmail.com>
2021-11-21 13:30:13 +0100

    Changed phone interface to a class so it can be created and initialized when creating a new phone

--- client/src/app/models/phone.ts
+++ client/src/app/models/phone.ts
@@ -1,4 +1,4 @@
-export interface Phone {
+export class Phone {
   id?: number;
   name?: string;
   manufacturer?: string;
@@ -9,4 +9,17 @@ export interface Phone {
   screen?: string;
   processor?: string;
   ram?: number;
+
+  constructor() {
+    this.id = null!;
+    this.name = '';
+    this.manufacturer = '';
+    this.description = '';
+    this.color = '';
+    this.price = 0;
+    this.imageFileName = 'IPhone_7.png';
+    this.screen = '';
+    this.processor = '';
+    this.ram = 0;
+  }
 }
